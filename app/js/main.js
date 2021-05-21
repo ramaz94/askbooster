4dde109b599af264e15ec7f3d74167853c48230c
ramaz94 <ramaz635@gmail.com>
2021-05-21 15:40:44 +0300

    added sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function(){
-    
+
+
+  $('.revive__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    prevArrow: '<button class="slick-arrow-prew" type="button"><img src="./img/arrow-left.svg"></button>',
+    nextArrow: '<button class="slick-arrow-next" type="button"><img src="./img/arrow-right.svg"></button>'
+  });
+
 
 
 });
+
+
+
