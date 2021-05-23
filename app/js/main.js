8a90073b1f6242b9b685b687ea5ea7a917b9695a
ramaz94 <ramaz635@gmail.com>
2021-05-23 20:27:54 +0300

    added footer

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,33 @@ $(function(){
     nextArrow: '<button class="slick-arrow-next" type="button"><img src="./img/arrow-right.svg"></button>'
   });
 
+  $('.title-1').hover(function(){
+    $('.title_top-1, .boost__top-text, .boost__top-title').addClass('hovered');
+  }, function() {
+    $('.title_top-1,.boost__top-text, .boost__top-title').removeClass('hovered');
+  });
+
+  $('.title-2').hover(function(){
+    $('.title_top-2, .boost__top-text, .boost__top-title').addClass('hovered');
+  }, function() {
+    $('.title_top-2,.boost__top-text, .boost__top-title').removeClass('hovered');
+  });
+
+
+  $('.title-3').hover(function(){
+    $('.title_top-3, .boost__top-text, .boost__top-title').addClass('hovered');
+  }, function() {
+    $('.title_top-3,.boost__top-text, .boost__top-title').removeClass('hovered');
+  });
+
+
+  $('.title-4').hover(function(){
+    $('.title_top-4, .boost__top-text, .boost__top-title').addClass('hovered');
+  }, function() {
+    $('.title_top-4,.boost__top-text, .boost__top-title').removeClass('hovered');
+  });
+
+
 
 
 });
